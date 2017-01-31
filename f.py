b7b3353acbe270c9d8130416557062f607e491ca
altg0x0 <altg0x0@yandex.ru>
2017-01-31 19:13:53 +0300

    Added correct F task.

--- f.py
+++ f.py
@@ -3,21 +3,20 @@ delim = '**********'
 n = int(input())
 arr = []
 for i in range(n):
-    arr.append(int(input()))
+    arr.append(input())
 buckets = [[] for _ in range(10)]
 print("Initial array:")
 print(*arr, sep=', ')
 print(delim)
-for i in range(1, len(str(arr[0])) + 1): # log10 would be better
+for i in range(1, len(arr[0]) + 1):
     for j in arr:
-        buckets[j % 10 ** i // (10 ** (i - 1))].append(j)
+        buckets[int(j[-i])].append(j)
     print('Phase ' + str(i))
     for t in range(10):
         print('Bucket ' + str(t) + ': ', end='')
-        print(', '.join(map(str, buckets[t])) if buckets[t] else 'empty')
-        print(delim)
+        print(', '.join(buckets[t]) if buckets[t] else 'empty')
+    print(delim)
     arr = list(itertools.chain(*buckets))
     buckets = [[] for _ in range(10)]
-print(delim)
 print("Sorted array:")
 print(*arr, sep=', ')
