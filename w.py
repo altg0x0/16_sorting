bcbebf4af40c7a59a332cc5fc0543d3ad8b8e011
altg0x0 <altg0x0@yandex.ru>
2017-02-08 15:55:25 +0300

    Added W task.

--- w.py
+++ w.py
@@ -1,11 +1,18 @@
 n = int(input())
-lp = []
-rp = []
+points = []
 for __ in range(n):
     inp = list(map(int, input().split()))
-    lp.append(inp[0])
-    rp.append(inp[1])
-lp.sort()
-rp.sort()
-for i in points:
-    print(bisect(lp, i) - bisect_left(rp, i))
+    points.append((inp[0], 1))
+    points.append((inp[1], -1))
+points.sort()
+pgen = (i for i in points)
+seg = 0
+p = 0
+col = 0
+for __ in range(2 * n):
+    x = next(pgen)
+    pnew = x[0]
+    col += (pnew - p) if seg > 0 else 0
+    seg += x[1]
+    p = pnew
+print(col)
