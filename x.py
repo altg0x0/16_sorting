5fc5f0cfaa8add83301d2eb5da9604fbc88c032a
altg0x0 <altg0x0@yandex.ru>
2017-02-10 20:16:11 +0300

    Task X, correct approach, but checker is full of carp.

--- x.py
+++ x.py
@@ -9,16 +9,16 @@ while inp_raw != '0 0':
     inp_raw = input()
 segs.sort()
 last = 0
+ind = -1
 cover = []
 lefts = list(map(lambda x: x[0], segs))
-lmi = -1
-while last == -1 or last < m:
+while last < m:
     max_ind = bisect_left(lefts, last + 1) - 1
-    if lmi == max_ind or segs[max_ind][0] > last + 1 or segs[max_ind][0] == segs[max_ind][1]:
+    if ind == max_ind:
         print('No solution')
         sys.exit()
-    last = segs[max_ind][1]
-    lmi = max_ind
-    cover.append(segs[max_ind])
+    ind = max(range(ind + 1, max_ind + 1), key=lambda x: segs[x][1])
+    last = segs[ind][1]
+    cover.append(segs[ind])
 print(len(cover))
 print(*(' '.join(map(str, x)) for x in cover), sep='\n')
